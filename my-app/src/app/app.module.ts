053b85e5ad6e58f7e69370e055466f41d659bd16
Summer <Summerrichards01@gmail.com>
2019-09-16 12:58:22 -0500

    styling toolbar component

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
