8f78d77c9c2808b1533d375fddfd5f3401b7b566
Summer <Summerrichards01@gmail.com>
2019-09-17 12:30:53 -0500

    adding navigation and making navigation movile responsive

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
