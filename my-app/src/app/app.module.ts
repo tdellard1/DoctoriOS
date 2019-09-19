d9ec84bc7d1b48c4c977aed7565d9e588c539797
Summer <Summerrichards01@gmail.com>
2019-09-19 12:53:40 -0500

    adding social media link to home page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
