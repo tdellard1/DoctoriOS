4998ce0b720bf2f4218143d4930ef8491ae850f0
Summer <Summerrichards01@gmail.com>
2019-09-19 23:06:02 -0500

    adding social media link to home page

--- my-app/src/app/home-page/home-page.component.ts
+++ my-app/src/app/home-page/home-page.component.ts
@@ -12,7 +12,21 @@ export class HomePageComponent implements OnInit {
   ngOnInit() {
   }
 
-
+showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < dots.length; i++) {
+    slides[i];
+  }
+ var slidesIndex;
+ if (slidesIndex > slides.length) {slidesIndex = 1;}
+ for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace("active", 
+'');
+ }
+ setTimeout(this.showSlides, 2000);
+}
 
 
 
