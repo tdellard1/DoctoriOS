b975167e40e24c75513651eefd4cac8aea554813
Travis Dellard <travisdellard@Traviss-iMac.attlocal.net>
2019-09-27 23:26:14 -0500

    Removing code that would effectively put project in default mode

--- my-app/src/app/app.component.spec.ts
+++ my-app/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('Doctor iOS');
   });
 
   it('should render title', () => {
