ad8f5439c59dd8bfb9b9b233f72267b772049561
Summer <Summerrichards01@gmail.com>
2019-10-01 18:32:34 -0500

    styling toolbar

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ToolbarComponent} from './toolbar/toolbar.component'
+import { ToolbarComponent} from './toolbar/toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ToolbarComponent} from './toolbar/toolbar.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
