49bf0ba658b2d2222a9384b43a7aac0d2a2538e6
tdellard1 <joijanise.01>
2019-10-04 18:01:45 -0500

    making home page component mobile responsive

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  countDown = new Date('October 6, 2019 21:00:25').getTime();
+  timer = '';
+  constructor() {
   }
 
+
+
+
+// tslint:disable-next-line:prefer-const align no-unused-expression
+
+  ngOnInit() {
+    setInterval(() => {
+      const now = new Date().getTime();
+      const distance = this.countDown - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.timer = `${days}days ${hours}hours ${minutes}minutes ${seconds}seconds`;
+      }, 1000);
+  }
 }
