7fc01a80a562594391f4002ad83e18bed6e52369
Summer <Summerrichards01@gmail.com>
2019-10-29 13:10:41 -0500

    adding appointment component

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppointmentPageComponent } from './appointment-page/appointment-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+
+  {path:'', 
+component: AppointmentPageComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppointmentPageComponent } from './appointment-page/appointment-page.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatButtonModule, MatIconModule, MatMenuModule, MatInputModule, MatSelectModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppointmentPageComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
